f9b36af6e6dd0ce54c70c10ef515185ae7f884cc
Ricky Sanchez <rickysanchez4130@gmail.com>
2018-12-12 22:02:49 -0800

    Added sudo to each command

--- docker/setup.sh
+++ docker/setup.sh
@@ -5,29 +5,29 @@ set -euxo pipefail
 # Initial server setup for Ubuntu 18.04
 
 # Creating user
-adduser myuser --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password
+sudo adduser myuser --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password
 echo "myuser:password" | chpasswd
 
 # Adding the new user to the sudo group
-usermod -aG sudo myuser
+sudo usermod -aG sudo myuser
 
 # Allow firewall SSH connections
-ufw allow OpenSSH
+sudo ufw allow OpenSSH
 
 # Enable the firewall
-echo "y" | ufw enable
+sudo echo "y" | ufw enable
 
 # Add official Docker GPG key
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
 # Add Docker repo to APT
-add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
 # Update APT
-apt-get update
+sudo apt-get update
 
 # Docker install
-apt-get install -y docker-ce
+sudo apt-get install -y docker-ce
 
 # Pull Ubuntu Image for docker
 sudo docker pull ubuntu

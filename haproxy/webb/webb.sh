12c6eaab5ea4a2a4f5aa089131521ae7c25b6344
Ricky Sanchez <rickysanchez4130@gmail.com>
2019-05-07 17:45:03 -0700

    web server b

---
+++ haproxy/webb/webb.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -e
+set -o
+
+# Update apt and install Nginx
+sudo apt update
+sudo apt install nginx -y
+
+# Create index.html containg "a"
+echo "b" > /etc/html/www/index.html

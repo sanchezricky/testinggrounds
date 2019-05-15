5ad351188e832cb7036c05806f1a34479b9128e7
Ricky Sanchez <rickysanchez4130@gmail.com>
2019-05-14 23:14:43 -0700

    frontend backend config

--- haproxy/haproxy/hap.sh
+++ haproxy/haproxy/hap.sh
@@ -5,3 +5,13 @@ sudo apt update
 sudo apt install haproxy -y
 
 #edit haproxy cfg file
+echo "
+frontend www
+    bind haproxyIP:80
+    default_backend webbers
+
+backend webbers
+    balance roundrobin
+    mode http
+    server weba web_a_IP:80 check
+    server webb web_b_IP:80 check" | sudo tee --append /etc/haproxy/haproxy.cfg

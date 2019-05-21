c409092ed6545baa9da6c18d413a9abc3140c234
Ricky Sanchez <rickysanchez4130@gmail.com>
2019-05-21 00:39:37 -0700

    Sticky sessions

--- haproxy/haproxy/hap.sh
+++ haproxy/haproxy/hap.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-# update apt and install haproxy
+# Update apt and install haproxy
 sudo apt update
 sudo apt install haproxy -y
 
-#edit haproxy cfg file
+# Edit haproxy cfg file
 echo "
 frontend www
     bind 192.168.56.30:80
@@ -13,5 +13,9 @@ frontend www
 backend webbers
     balance roundrobin
     mode http
-    server weba 192.168.56.20:80 check
-    server webb 192.168.56.10:80 check" | sudo tee --append /etc/haproxy/haproxy.cfg
+    cookie SERVERID insert indirect nocache
+    server s1 192.168.56.20:80 check cookie s1
+    server s2 192.168.56.10:80 check cookie s2" | sudo tee --append /etc/haproxy/haproxy.cfg
+
+# Restarting Haproxy
+sudo systemctl restart haproxy
